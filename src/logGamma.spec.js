8d4f389f6a191e0f5f742bace575731a6c10bd78
Zetos Lima <cesar_manoel@hotmail.com>
2017-02-28 20:36:06 -0300

    Added tests for logGamma.spec.js

--- src/logGamma.spec.js
+++ src/logGamma.spec.js
@@ -14,4 +14,40 @@ describe('logGamma', function() {
             expect(logGamma).to.be.a('function');
         });
     });
+
+    describe('the function returns the logarithm of the gamma function', function() {
+        it('should return 1.20097360234 for the input 3.5', ()=>{
+            expect(logGamma(3.5)).to.equal(1.2009736023470738);
+        });
+
+        it('should return 9.210282658633963 for the input 0.0001', ()=>{
+            expect(logGamma(0.0001)).to.equal(9.210282658633963);
+        });
+
+        it('should return 3.1780538303479453 for the input 5', ()=>{
+            expect(logGamma(5)).to.equal(3.1780538303479453);
+        });
+
+        it('should return 0 for the input 1', ()=>{
+            expect(logGamma(5)).to.equal(0);
+        });
+
+        it('should return 0 for the input 2', ()=>{
+            expect(logGamma(5)).to.equal(0);
+        });
+
+        it('should return +Infinity for the input 0', ()=>{
+            expect(logGamma(0)).to.equal(Infinity);
+        });
+    });
+
+    describe('error tests', function() {
+        it('should return a error for a NaN input', ()=>{
+            expect(logGamma('Not a number')).to.equal(NaN);
+        });
+
+        it('should return a error for a negative number', ()=>{
+            expect(logGamma(-666)).to.equal(NaN);
+        });
+    });
 });
