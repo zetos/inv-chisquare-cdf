eedd875bc2301111714f20a28c9794c5dc0b8ad3
Zetos Lima <cesar_manoel@hotmail.com>
2017-02-28 18:57:28 -0300

    plugin for mocha added in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,13 @@ module.exports = {
     "extends": ["eslint:recommended", "google"],
     "env":{
         "es6": true,
+        "node": true
     },
+    "plugins": [
+        "mocha"
+    ],
     "rules": {
          "max-len": [1, 80, 2],
+         "mocha/no-exclusive-tests": "error",
     }
 };
