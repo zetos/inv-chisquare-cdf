65412c61a10c3b597afcb5cac8a2f0aede12b147
Zetos Lima <cesar_manoel@hotmail.com>
2017-03-01 19:53:53 -0300

    Added test for lower regularized gamma function

---
+++ src/regLowGamma.spec.js
@@ -0,0 +1,48 @@
+/* eslint-env mocha*/
+
+const regLowGamma = require('./regLowGamma.js');
+const expect = require('chai').expect;
+
+describe('regLowGamma', function() {
+	describe('smoke tests', function() {
+		it('should exist', ()=>{
+			expect(regLowGamma).to.exist;
+		});
+
+		it('should be a function', ()=>{
+			expect(regLowGamma).to.be.a('function');
+		});
+	});
+
+	describe('the function returns the lower regularized incomplete gamma function evaluated at (a,x)', function() {
+		it('should return 0.5595067149347875 for a=5 and x=5', ()=>{
+			expect(regLowGamma(5, 5)).to.equal(0.5595067149347875);
+		});
+
+		it('should return 0.9987538088133204 for a=0.666 and x=5.75', ()=>{
+			expect(regLowGamma(5, 0)).to.equal(0.9987538088133204);
+		});
+
+		it('should return 0 for a=5 and x=0', ()=>{
+			expect(regLowGamma(5, 0)).to.equal(0);
+		});
+	});
+
+	describe('error tests', function() {
+		it('should return a error for a NaN input in the parameter: a', ()=>{
+			expect(()=>regLowGamma('Not a number')).to.throw('The value is not a number.');
+		});
+
+        it('should return a error for a NaN input in the parameter: x', ()=>{
+			expect(()=>regLowGamma('Not a number')).to.throw('The value is not a number.');
+		});
+
+		it('should return a error for a number lower or equal to 0 in the parameter: a', ()=>{
+			expect(()=>regLowGamma(0, 2)).to.throw('The value is a negative number.');
+		});
+
+        it('should return a error for a negative number in the parameter: x', ()=>{
+			expect(()=>regLowGamma(-666, 2)).to.throw('The value is a negative number.');
+		});
+	});
+});
