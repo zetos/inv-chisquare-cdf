0da99666da578ca93bfa35b651ba2c61cc95021c
Zetos Lima <cesar_manoel@hotmail.com>
2017-02-28 22:06:01 -0300

    Tests of logGamma.js changes
    Fixes on tests for the input '1' and '2'. Error throw error added on the error tests.

--- src/logGamma.spec.js
+++ src/logGamma.spec.js
@@ -5,49 +5,49 @@ const logGamma = require('./logGamma.js');
 const expect = require('chai').expect;
 
 describe('logGamma', function() {
-    describe('smoke tests', function() {
-        it('should exist', ()=>{
-            expect(logGamma).to.exist;
-        });
+	describe('smoke tests', function() {
+		it('should exist', ()=>{
+			expect(logGamma).to.exist;
+		});
 
-        it('should be a function', ()=>{
-            expect(logGamma).to.be.a('function');
-        });
-    });
+		it('should be a function', ()=>{
+			expect(logGamma).to.be.a('function');
+		});
+	});
 
-    describe('the function returns the logarithm of the gamma function', function() {
-        it('should return 1.20097360234 for the input 3.5', ()=>{
-            expect(logGamma(3.5)).to.equal(1.2009736023470738);
-        });
+	describe('the function returns the logarithm of the gamma function', function() {
+		it('should return 1.20097360234 for the input 3.5', ()=>{
+			expect(logGamma(3.5)).to.equal(1.2009736023470738);
+		});
 
-        it('should return 9.210282658633963 for the input 0.0001', ()=>{
-            expect(logGamma(0.0001)).to.equal(9.210282658633963);
-        });
+		it('should return 9.210282658633963 for the input 0.0001', ()=>{
+			expect(logGamma(0.0001)).to.equal(9.210282658633963);
+		});
 
-        it('should return 3.1780538303479453 for the input 5', ()=>{
-            expect(logGamma(5)).to.equal(3.1780538303479453);
-        });
+		it('should return 3.1780538303479453 for the input 5', ()=>{
+			expect(logGamma(5)).to.equal(3.1780538303479453);
+		});
 
-        it('should return 0 for the input 1', ()=>{
-            expect(logGamma(5)).to.equal(0);
-        });
+		it('should return 0 for the input 1', ()=>{
+			expect(logGamma(1)).to.equal(0);
+		});
 
-        it('should return 0 for the input 2', ()=>{
-            expect(logGamma(5)).to.equal(0);
-        });
+		it('should return 0 for the input 2', ()=>{
+			expect(logGamma(2)).to.equal(0);
+		});
 
-        it('should return +Infinity for the input 0', ()=>{
-            expect(logGamma(0)).to.equal(Infinity);
-        });
-    });
+		it('should return +Infinity for the input 0', ()=>{
+			expect(logGamma(0)).to.equal(Infinity);
+		});
+	});
 
-    describe('error tests', function() {
-        it('should return a error for a NaN input', ()=>{
-            expect(logGamma('Not a number')).to.equal(NaN);
-        });
+	describe('error tests', function() {
+		it('should return a error for a NaN input', ()=>{
+			expect(()=>logGamma('Not a number')).to.throw('The value is not a number.');
+		});
 
-        it('should return a error for a negative number', ()=>{
-            expect(logGamma(-666)).to.equal(NaN);
-        });
-    });
+		it('should return a error for a negative number', ()=>{
+			expect(()=>logGamma(-666)).to.throw('The value is a negative number.');
+		});
+	});
 });
