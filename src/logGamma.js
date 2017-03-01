82a36bbd7d4d8511da3e4455218ae7a26a46d201
Zetos Lima <cesar_manoel@hotmail.com>
2017-02-28 22:07:20 -0300

    logGamma function

---
+++ src/logGamma.js
@@ -0,0 +1,36 @@
+module.exports = function logGamma(x) {
+    if(x===1 || x===2) {
+        return 0;
+    } else if(x===0) {
+        return Infinity;
+    } else if(isNaN(x)) {
+        // booleans and strings like '123' will not fall here.
+        throw new Error(`The value is not a number.`);
+        // throw new Error('msg');
+    } else if(x<0) {
+        throw new Error(`The value is a negative number.`);
+    }
+
+    // Lanczos approximation
+    const cof = [
+    76.18009172947146,
+    -86.50532032941677,
+    24.01409824083091,
+    -1.231739572450155,
+    0.1208650973866179e-2,
+    -0.5395239384953e-5,
+    ];
+    let ser = 1.000000000190015;
+
+    let xx;
+    let y;
+    let tmp;
+    tmp = (y = xx = x) + 5.5;
+    tmp -= (xx + 0.5) * Math.log(tmp);
+
+    cof.map((approximation)=>{
+        ser += approximation / ++y;
+    });
+
+    return Math.log(2.5066282746310005 * ser / xx) - tmp;
+};
