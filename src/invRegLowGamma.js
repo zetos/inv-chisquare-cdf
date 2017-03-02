563a76ce1e98341bf83eb12ab88a98ad84dc99ce
Zetos Lima <cesar_manoel@hotmail.com>
2017-03-02 20:21:33 -0300

    comments remove on invRegLowGamma

--- src/invRegLowGamma.js
+++ src/invRegLowGamma.js
@@ -14,8 +14,6 @@ module.exports = function invRegLowGamma(p, a) {
         return 0;
     }
 
-    // return 'satanic';
-
     let a1 = a - 1;
     let EPS = 1e-8;
     let gln = logGamma(a);
@@ -63,54 +61,3 @@ module.exports = function invRegLowGamma(p, a) {
 
     return inverseRegLowGamma;
 };
-
-
-// function gammapinv(p, a) {
-    //   let j = 0;
-    //   let a1 = a - 1;
-    //   let EPS = 1e-8;
-    //   let gln = jStat.gammaln(a);
-    //   let inverseRegLowGamma, err, t, u, pp, lna1, afac;
-
-    //   if (p >= 1)
-    //     return Math.max(100, a + 100 * Math.sqrt(a));
-    //   if (p <= 0)
-    //     return 0;
-    //   if (a > 1) {
-        //     lna1 = Math.log(a1);
-        //     afac = Math.exp(a1 * (lna1 - 1) - gln);
-        //     pp = (p < 0.5) ? p : 1 - p;
-        //     t = Math.sqrt(-2 * Math.log(pp));
-        //     inverseRegLowGamma = (2.30753 + t * 0.27061) / (1 + t * (0.99229 + t * 0.04481)) - t;
-        //     if (p < 0.5)
-        //       inverseRegLowGamma = -inverseRegLowGamma;
-        //     inverseRegLowGamma = Math.max(1e-3,
-        //                  a * Math.pow(1 - 1 / (9 * a) - inverseRegLowGamma / (3 * Math.sqrt(a)), 3));
-        //   } else {
-            //     t = 1 - a * (0.253 + a * 0.12);
-            //     if (p < t)
-            //       inverseRegLowGamma = Math.pow(p / t, 1 / a);
-            //     else
-            //       inverseRegLowGamma = 1 - Math.log(1 - (p - t) / (1 - t));
-            //   }
-
-            //   for(; j < 12; j++) {
-                //     if (inverseRegLowGamma <= 0)
-                //       return 0;
-                //     err = lowRegGamma(a, inverseRegLowGamma) - p;
-                //     if (a > 1)
-                //       t = afac * Math.exp(-(inverseRegLowGamma - a1) + a1 * (Math.log(inverseRegLowGamma) - lna1));
-                //     else
-                //       t = Math.exp(-inverseRegLowGamma + a1 * Math.log(inverseRegLowGamma) - gln);
-                //     u = err / t;
-                //     inverseRegLowGamma -= (t = u / (1 - 0.5 * Math.min(1, u * ((a - 1) / inverseRegLowGamma - 1))));
-                //     if (inverseRegLowGamma <= 0)
-                //       inverseRegLowGamma = 0.5 * (inverseRegLowGamma + t);
-                //     if (Math.abs(t) < EPS * inverseRegLowGamma)
-                //       break;
-                //   }
-
-                //   return inverseRegLowGamma;
-                // };
-
-
