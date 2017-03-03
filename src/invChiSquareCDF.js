3eed25e2954ac86fe10b9a7a3ef01f8e93d4b2df
Zetos Lima <cesar_manoel@hotmail.com>
2017-03-02 21:22:57 -0300

    invChiSquareCDF function

---
+++ src/invChiSquareCDF.js
@@ -0,0 +1,21 @@
+const invRegLowGamma = require('./invRegLowGamma.js');
+
+module.exports = function invChiSquareCDF(probability, degreeOfFreedom) {
+    if(isNaN(probability)) {
+        // TODO fix: booleans and strings like '123' will not fall here.
+        throw new Error('The value in param "probability" is not an number.');
+    } else if(isNaN(degreeOfFreedom)) {
+        // TODO fix: booleans and strings like '123' will not fall here.
+        throw new Error('The value in param "degreeOfFreedom" is not an number.');
+    } else if (probability >= 1 || probability <= 0) {
+        throw new Error('The number in param "probability" must lie in the interval [0 1].');
+    } else if (degreeOfFreedom <= 0) {
+        throw new Error('The number in param "degreeOfFreedom" must be greater than 0.');
+    }
+
+    return 2 * invRegLowGamma(probability, 0.5 * degreeOfFreedom);
+};
+
+// inv: function(p, dof) {
+//     return 2 * jStat.gammapinv(p, 0.5 * dof);
+//   }
